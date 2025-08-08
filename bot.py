1b31c99abbc3426c5e5799ac29e73a8a24b2f07b
Overdozz123 <araratbass@gmail.com>
2025-08-08 16:22:03 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, F
+from handlers import start, select_serial, handle_photo, approve_reject
+from database import init_db
 
-TOKEN = "8218677693:AAGinXaUWvXP-QafxKWLF-4gYnKgnGoMBDs"
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-@dp.message(commands=["start"])
-async def cmd_start(message: Message):
-    await message.answer("Բարև, Render-ից աշխատող aiogram բոտ եմ։")
+TOKEN = '8200873228:AAGLKVU0BoeZSok9m_SvaQNh81xc7fyHOns'
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    init_db()
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher(bot)
 
-if __name__ == "__main__":
+    dp.message.register(start, commands="start")
+    dp.callback_query.register(select_serial, lambda c: c.data and c.data.startswith("serial_"))
+    dp.message.register(handle_photo, F.photo)
+    dp.callback_query.register(approve_reject, lambda c: c.data and (c.data.startswith("approve_") or c.data.startswith("reject_")))
+
+    print("Bot is running...")
+    await dp.start_polling()
+
+if __name__ == '__main__':
     asyncio.run(main())
