29ad0a15836f9d2b8b57714cb94cfcec370603c2
Overdozz123 <araratbass@gmail.com>
2025-08-08 15:22:00 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Քո ուղարկած Bot Token-ը տեղադրված է այստեղ
+TOKEN = "8218677693:AAGinXaUWvXP-QafxKWLF-4gYnKgnGoMBDs"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Բարև, Render-ից աշխատող մշտական բոտ եմ։")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
